250d998b1da830dc5f0b784dcfe22eb4325e1b9c
Paula Iazmín Pereda Suárez <33704279+paulapereda@users.noreply.github.com>
2025-05-02 16:49:29 -0300

    Update master_extract.R

--- src/master_extract.R
+++ src/master_extract.R
@@ -1,11 +1,9 @@
 pacman::p_load(tidyverse, rvest, here, httr)
 
-# (!) CAMBIAR EL 01 AL 09 PARA NO SOBREESCRIBIR ARCHIVOS 
-
 ## (1) IMAGES
 
 # Imágenes - Vestimenta, calzado, carteras, joyería, belleza
-# source(here("src", "extract-images-previa", "extract-previa.R"))
+ source(here("src", "extract-images-previa", "extract-previa.R"))
 
 ## (2) INFORMATION
 
@@ -24,16 +22,6 @@ source(here("src", "extract-info-previa", "extract-previa-joyeria.R"))
 # Info - Belleza
 source(here("src", "extract-info-previa", "extract-previa-belleza.R"))
 
-### Code:
-#### - 01: previa moweek ✓
-#### - 02: domingo pre moweek ✓
-#### - 03: lunes pre moweek ✓
-#### - 04: martes pre moweek ✓
-#### - 05: miércoles pre moweek (bug en el código para info)
-#### - 06: jueves pre moweek 
-#### - 07: viernes de moweek (día 1)
-#### - 08: sábado de moweek (día 2)
-#### - 09: domingo de moweek (día 3)
 
 
 
